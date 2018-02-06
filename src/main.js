10c1240b0191a8642eb7eb0c64f6750ada225c9e
sonnyg <sonny@evagabond.com>
2018-02-06 09:48:31 -0800

    adding range input and update handlers

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-console.log('I am alive!')
+const redSlider = document.getElementById('redSlider')
+const greenSlider = document.getElementById('greenSlider')
+const blueSlider = document.getElementById('blueSlider')
+
+redSlider.addEventListener('input', updateRedValue, false)
+greenSlider.addEventListener('input', updateGreenValue, false)
+blueSlider.addEventListener('input', updateBlueValue, false)
+
+const redValue = document.getElementById('redValue')
+const greenValue = document.getElementById('greenValue')
+const blueValue = document.getElementById('blueValue')
+
+updateRedValue()
+updateGreenValue()
+updateBlueValue()
+
+function updateRedValue() {
+  redValue.innerText = redSlider.value
+}
+
+function updateGreenValue() {
+  greenValue.innerText = greenSlider.value
+}
+
+function updateBlueValue() {
+  blueValue.innerText = blueSlider.value
+}
